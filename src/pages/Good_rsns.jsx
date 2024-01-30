3736c326f282180312013c60ee01eb0d2a2eac7a
Lautaro <lautaro.agustint@gmail.com>
2024-01-30 12:10:26 -0300

    no ser lo mejor

--- src/pages/Good_rsns.jsx
+++ src/pages/Good_rsns.jsx
@@ -6,51 +6,55 @@ function Good_rsns() {
     const [razones, setRazones] = useState([
     {
         id: 1,
-        razon: "Siempre nos vas a querer"
+        razon: "Porque incluso no siendo los mejores hijos, siempre nos vas a querer por mas que nos pelemos."
     },
     {
-        id:2 ,
-        razon: "Siempre nos vas a cuidar"
+        id: 2,
+        razon: "Siempre nos vas a querer."
     },
     {
         id: 3,
-        razon: "Siempre nos das lo que necesitamos y mas"
+        razon: "Siempre nos vas a cuidar."
     },
     {
         id: 4,
-        razon: "Siempre nos dejas salir"
+        razon: "Siempre nos das lo que necesitamos y mas."
     },
     {
         id: 5,
-        razon: "Siempre nos vas a apoyar en las decisiones que tomemos"
+        razon: "Siempre nos dejas salir."
     },
     {
         id: 6,
-        razon: "Siempre nos siempre nos vas a ayudar en lo que necesitemos"
+        razon: "Siempre nos vas a apoyar en las decisiones que tomemos."
     },
     {
         id: 7,
-        razon: "Siempre nos vas a dar los mejores consejos que nos puedas dar"
+        razon: "Siempre nos siempre nos vas a ayudar en lo que necesitemos."
     },
     {
         id: 8,
-        razon: "Siempre nos vas a tener una paciencia infinita en todo"
+        razon: "Siempre nos vas a dar los mejores consejos que nos puedas dar."
     },
     {
         id: 9,
-        razon: "Siempre nos enzeñaste los mejores valores"
+        razon: "Siempre nos vas a tener una paciencia infinita en todo."
     },
     {
         id: 10,
-        razon: "Siempre nos vas a celebrar todos los exitos que tengamos"
+        razon: "Siempre nos enzeñaste los mejores valores."
     },
     {
         id: 11,
-        razon: "Siempre vas a estar con nosotros"
+        razon: "Siempre nos vas a celebrar todos los exitos que tengamos."
     },
     {
         id: 12,
-        razon: "Siempre nos vas a dar comidas mas ricas que la de los restaurantes"
+        razon: "Siempre vas a estar con nosotros."
+    },
+    {
+        id: 13,
+        razon: "Siempre nos vas a dar comidas mas ricas que la de los restaurantes."
     }
 ]);
     
